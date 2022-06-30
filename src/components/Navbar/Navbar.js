786929f057515187854d06f76f0aac45858006e2
Lucas Peñalba <lucaspenalba@gmail.com>
2022-06-29 22:04:44 -0300

    ultima parte

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import BottonNav from '../BottomNav/BottonNav';
 import CartIcon from '../Carticon/CartIcon';
 import { NavLink } from 'react-router-dom'
+
 
 
 export default function Header () {
@@ -10,22 +10,28 @@ export default function Header () {
 
 
     return (
+        
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" expand="lg">
                 <Container>
                     <Navbar.Brand>
-                        <NavLink to="/">Alimento de Mascotas</NavLink>
+                        <NavLink to="/">                        
+                            Alimento de Mascotas
+                        </NavLink>
                     </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link><NavLink to="/">Inicio</NavLink></Nav.Link>
                         <Nav.Link><NavLink to="/products">Productos</NavLink></Nav.Link>
-                        <Nav.Link><NavLink to="/category/1">Categoria 1</NavLink></Nav.Link>
-                        <Nav.Link><NavLink to="/category/2">Categoria 2</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to="/category/1">Perros</NavLink></Nav.Link>
+                        <Nav.Link><NavLink to="/category/2">Gatos</NavLink></Nav.Link>
                     </Nav>
                     <CartIcon/>
+                    </Navbar.Collapse>
+                    
                 </Container>
             </Navbar>
-            <BottonNav/>
         
         </>
 
--- src/view/Checkout/Checkout.js
+++ src/view/Checkout/Checkout.js
@@ -1,104 +1,190 @@
 import React, { useState, useEffect }  from 'react'
+import { Form, Button, Container } from 'react-bootstrap';
 import { CartContext } from "../../context/CartContext";
 
+
 import {
+
     doc,
+
     addDoc,
+
     runTransaction,
+
     collection,
+
     getFirestore,
+
   } from "firebase/firestore";
+
+import { useNavigate } from 'react-router-dom';
+
 
 
 export default function Checkout(){
 
+
     const [data, setData] = React.useState()
+
   console.log(data)
-  const [orderId, setOrderId] = React.useState()
+
+  const [orderId, setOrderId] = React.useState(false)
+
   console.log(orderId)
 
-  const {cart}= React.useContext(CartContext);
+  const navegar = useNavigate()
+
+  const {cart, deleteAll}= React.useContext(CartContext);
+
 
   const [price, setPrice] = useState(0);
 
+
     const handlePrice = () => {
+
         let ans = 0;
+
         cart.map((item) => (ans += item.quantity * item.price));
+
         setPrice(ans);
+
       };
 
+
       useEffect(() => {
+
         handlePrice();
+
       });
 
 
+
   const handleChange = (event) => {
+
     const { name, value } = event.target;
+
     setData({ ...data, [name]: value });
+
   }
+
 
   const handleSubmit = async (event) => {
+
     event.preventDefault();
+
     const order = {
+
       buyer: data,
+
       items: cart,
+
       total: price,
+
     };
+
     const db = getFirestore()
+
     const ordersCollection = collection(db, "orders")
+
     await addDoc(ordersCollection, order).then(({id}) => {
+
       setOrderId(id)
+
       updateProducts()
+
+      deleteAll()
+
     })
+
   }
 
+
   const updateProducts = async () => {
+
     const db = getFirestore ()
+
     cart.forEach( async (item) => {
+
       const productRef = doc(db, `productos`, item.id)
+
       await runTransaction(db, async (transaction) => {
+
       const transfDoc = await transaction.get(productRef);
+
       if (!transfDoc.exists()) {
+
         console.error("El documento no existe")
+
       }
+
       const newStock = transfDoc.data().stock - item.quantity;
+
       transaction.update(productRef, { stock: newStock });
+
     });
+
     })
+
   }
+
 
 
   return (
 
+
     
 
+
     <div className="form">
+
+      <Container>   
+
         
-      <form onSubmit={handleSubmit}>
-        <h1>Checkout</h1>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        <input
-          type="phone"
-          name="phone"
-          placeholder="Phone"
-          onChange={handleChange}
-        />
-        <input
-          type="submit"
-          value="Finalizar compra"
-        />
-      </form>
+
+      {!orderId 
+
+        ?<Form onSubmit={handleSubmit}  className="justify-content-md-center">
+
+          <h1>Checkout</h1>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Nombre y Apellido</Form.Label>
+            <Form.Control type="text" name="name" placeholder="Ingrese su nombre" onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Correo</Form.Label>
+            <Form.Control type="email" name="email" placeholder="Ingrese su correo" onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Telefono</Form.Label>
+            <Form.Control type="phone" name="phone" placeholder="Ingrese su Telefono" onChange={handleChange} />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+          Finalizar compra
+          </Button>
+
+
+          
+
+        </Form>
+
+        : <>
+
+          <h1>Muchas gracias por su compra!</h1>
+
+          <p>Su order {orderId} será despachada en 2 días hábiles</p>
+
+          <Button variant="primary" onClick={()=>navegar('/')}>Volver a Home</Button>
+
+          </>}
+
+          </Container>
+
     </div>
+
   );
+
 }
